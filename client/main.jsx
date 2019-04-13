43fd29fcfcc2b3211779c01831aa395b94168f5f
progmonster <progmonster@gmail.com>
2019-04-13 09:25:54 +0300

    Add Material Dashboard React

--- client/main.jsx
+++ client/main.jsx
@@ -2,21 +2,28 @@ import { Meteor } from 'meteor/meteor';
 import React from 'react';
 import { render } from 'react-dom';
 import Button from "@material-ui/core/Button";
+import ReactDOM from "react-dom";
+import { createBrowserHistory } from "history";
+import { Router, Route, Switch, Redirect } from "react-router-dom";
 
+// core components
+import Admin from "/imports/layouts/Admin.jsx";
+import RTL from "/imports/layouts/RTL.jsx";
+import "/imports/assets/css/material-dashboard-react.css?v=1.6.0";
 
 Meteor.startup(() => {
-  render(<HelloWorld />, document.getElementById('app'));
+  const hist = createBrowserHistory();
+
+  ReactDOM.render(
+    <Router history={hist}>
+      <Switch>
+        <Route path="/admin" component={Admin} />
+        <Route path="/rtl" component={RTL} />
+        <Redirect from="/" to="/admin/dashboard" />
+      </Switch>
+    </Router>,
+    document.getElementById("root")
+  );
 });
 
-
-class HelloWorld extends React.Component {
-  render() {
-    return (
-      <div>
-        <h1>Hello World!</h1>
-        <Button variant="contained" color="primary">Click Me!</Button>
-      </div>
-    );
-  }
-}
 
---
+++ imports/index.js
@@ -0,0 +1,20 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import { createBrowserHistory } from "history";
+import { Router, Route, Switch, Redirect } from "react-router-dom";
+
+
+import "assets/css/material-dashboard-react.css?v=1.6.0";
+
+const hist = createBrowserHistory();
+
+ReactDOM.render(
+  <Router history={hist}>
+    <Switch>
+      <Route path="/admin" component={Admin} />
+      <Route path="/rtl" component={RTL} />
+      <Redirect from="/" to="/admin/dashboard" />
+    </Switch>
+  </Router>,
+  document.getElementById("root")
+);
