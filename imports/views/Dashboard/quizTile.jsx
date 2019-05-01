ffe5ef5644e4f6abd3535437d7e85247dec571b3
progmonster <progmonster@gmail.com>
2019-05-01 14:01:08 +0300

    Customize quiz tile

--- imports/views/Dashboard/quizTile.jsx
+++ imports/views/Dashboard/quizTile.jsx
@@ -1,14 +1,28 @@
 import React from "react";
 import withStyles from "@material-ui/core/styles/withStyles";
-import AccessTime from "@material-ui/icons/AccessTime";
 import Card from "/imports/components/Card/Card.jsx";
-import CardHeader from "/imports/components/Card/CardHeader.jsx";
-import CardBody from "/imports/components/Card/CardBody.jsx";
-import CardFooter from "/imports/components/Card/CardFooter.jsx";
 import PropTypes from "prop-types";
-import dashboardStyle from "/imports/views/Dashboard/dashboardStyle.jsx";
 import { withTracker } from "meteor/react-meteor-data";
 import { Quizzes } from "../../collections";
+import IconButton from "@material-ui/core/IconButton";
+import CardActions from "@material-ui/core/CardActions";
+import CardContent from "@material-ui/core/CardContent";
+import Typography from "@material-ui/core/Typography";
+import Button from "@material-ui/core/Button";
+import { Delete, Edit } from "@material-ui/icons";
+import CardHeader from "@material-ui/core/CardHeader";
+import { orange } from "@material-ui/core/colors";
+
+const styles = theme => ({
+  headerRoot: {
+    backgroundColor: orange[500],
+  },
+
+  headerTitle: {
+    color: "white",
+    fontSize: 16,
+  }
+});
 
 class QuizTile extends React.PureComponent {
   render() {
@@ -16,19 +30,35 @@ class QuizTile extends React.PureComponent {
 
     return (
       <Card>
-        <CardHeader color="warning">
-          <h4 className={classes.cardTitleWhite}>{quiz.title}</h4>
-        </CardHeader>
-        <CardBody>
-          <p className={classes.cardCategory}>
-            Test yourself and get a gift!
-          </p>
-        </CardBody>
-        <CardFooter chart>
-          <div className={classes.stats}>
-            <AccessTime /> updated 4 minutes ago
-          </div>
-        </CardFooter>
+        <CardHeader
+          classes={{ root: classes.headerRoot, title: classes.headerTitle }}
+          title={quiz.title}
+        />
+
+        <CardContent>
+          <Typography component="p">
+            Lizards are a widespread group of squamate reptiles, with over 6,000 species, ranging
+            across all continents except Antarctica
+          </Typography>
+        </CardContent>
+
+        <CardActions>
+          <Button size="small" color="primary">
+            Learn
+          </Button>
+
+          <Button size="small" color="primary">
+            Quiz
+          </Button>
+
+          <IconButton>
+            <Edit />
+          </IconButton>
+
+          <IconButton color="secondary">
+            <Delete />
+          </IconButton>
+        </CardActions>
       </Card>
     );
   }
@@ -42,8 +72,7 @@ const QuizTileContainer = withTracker(({ quizId }) => {
   return {
     quiz: Quizzes.findOne(quizId)
   };
-})(withStyles(dashboardStyle)(QuizTile));
-
+})(withStyles(styles)(QuizTile));
 
 
 export default QuizTileContainer;
