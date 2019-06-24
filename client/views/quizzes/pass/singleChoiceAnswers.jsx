2807345043cdf2d1017bab309b83d8720fada81d
progmonster <progmonster@gmail.com>
2019-06-25 00:12:49 +0300

    UI: quiz pass

--- client/views/quizzes/pass/singleChoiceAnswers.jsx
+++ client/views/quizzes/pass/singleChoiceAnswers.jsx
@@ -1,24 +1,32 @@
 import React from 'react';
 import * as PropTypes from 'prop-types';
 import RadioGroup from '@material-ui/core/RadioGroup';
-import Grid from '@material-ui/core/Grid';
 import Radio from '@material-ui/core/Radio';
 import FormControlLabel from '@material-ui/core/FormControlLabel';
 import { green, red } from '@material-ui/core/colors';
 import { compose } from 'redux';
-import { withStyles } from '@material-ui/core';
+import { Typography, withStyles } from '@material-ui/core';
+import { Check, Clear } from '@material-ui/icons';
 
 const styles = {
+  answerLabel: {
+    display: 'flex',
+    alignItems: 'center',
+  },
+
+  uncheckedAnswer: {
+  },
+
   validAnswer: {
-    backgroundColor: green[50],
+    color: green[800],
   },
 
   correctUserAnswer: {
-    backgroundColor: green[500],
+    color: green[800],
   },
 
   incorrectUserAnswer: {
-    backgroundColor: red[500],
+    color: red[800],
   },
 };
 
@@ -68,25 +76,35 @@ const SingleChoiceAnswers = (
     />
   );
 
+  const getLabelWithAnswer = (title, checkedByUser, checked) => (
+    <Typography className={classes.answerLabel}>
+      {(checked && <span className={classes.correctUserAnswer}>{title}</span>)}
+      {(!checked && <span>{title}</span>)}
+
+      &nbsp;
+
+      {(!checked && !checkedByUser && <Check className={classes.correctUserAnswer} />)}
+      {(!checked && checkedByUser && <Clear className={classes.incorrectUserAnswer} />)}
+      {(checked && !checkedByUser && <Clear className={classes.incorrectUserAnswer} />)}
+      {(checked && checkedByUser && <Check className={classes.correctUserAnswer} />)}
+    </Typography>
+  );
+
   return (
-    <Grid container>
-      <Grid item xs={12} sm={12} md={8}>
-        <RadioGroup>
-          {answers.map(({ title, checked = false, checkedByUser = false }, answerIdx) => (
-            <FormControlLabel
-              classes={getAnswerClasses({
-                checked,
-                checkedByUser,
-              })}
-              key={answerIdx}
-              value={answerIdx.toString()}
-              label={title}
-              control={renderRadio(checkedByUser)}
-            />
-          ))}
-        </RadioGroup>
-      </Grid>
-    </Grid>
+    <RadioGroup>
+      {answers.map(({ title, checked = false, checkedByUser = false }, answerIdx) => (
+        <FormControlLabel
+          classes={getAnswerClasses({
+            checked,
+            checkedByUser,
+          })}
+          key={answerIdx}
+          value={answerIdx.toString()}
+          label={checkAnswer ? getLabelWithAnswer(title, checkedByUser, checked) : title}
+          control={renderRadio(checkedByUser)}
+        />
+      ))}
+    </RadioGroup>
   );
 };
 
